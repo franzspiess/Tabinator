c8b27a45b69d19766178dbb444fb209d90aace62
franzspiess <franz.spiess@gmail.com>
2020-05-02 08:13:21 +0200

    Commit current state

--- background.js
+++ background.js
@@ -1,21 +1,117 @@
-chrome.runtime.onInstalled.addListener(function() {
+const timeouts = {}
 
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.set({ duration: 1800000, 
+    openTabs: [] 
+  }, () => {
+  })
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: { schemes: ['http', 'https','chrome'] },
       })
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 
 });
 
-// chrome.tabs.onCreated.addListener((tab)=> {
-//   alert(JSON.stringify(tab))
-//   // setTimeout(()=> {
-//   //   chrome.tabs.remove(tab.id)
-//   // },1000)
-// } )
+
+chrome.tabs.onActivated.addListener((info) => {
+  console.log(info)
+  const tabId = info.tabId
+  chrome.storage.local.set({ currentTab: tabId }, () => {
+  })
+  chrome.storage.local.get(['duration', 'openTabs'], (result) => {
+    const { openTabs, duration } = result
+    console.log(duration, timeouts, openTabs, 'CCCCC')
+    chrome.windows.getCurrent({ populate: true }, (window) => {
+      window.tabs.forEach(tab => {
+        const domain = tab.url.match(/^(?:https?:)?(?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/i)
+        if (tab.active) {
+          console.log(tab.id, 'REMOVED')
+          clearTimeoutForTab(tab.id)
+        }
+        if (!tab.active && !openTabs.includes(tab.id) && !timeouts[tab.id]) {
+          console.log(tab.id, 'ADDED')
+          setTimeoutForTab(tab.id, duration)
+        }
+
+      })
+    })
+  })
+})
+
+// chrome.tabs.onCreated.addListener(({ id }) => {
+//   chrome.storage.local.get(['duration'], (result) => {
+//     console.log('CREATED')
+//     setTimeoutForTab(id, result.duration)
+//   })
+// })
+
+chrome.tabs.onRemoved.addListener((id) => {
+  if (timeouts[id]) {
+    clearTimeoutForTab(id)
+  }
+})
+
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    if (request.keepOpen) {
+      checkboxClick(sendResponse)
+    }
+    if (request.duration) {
+      console.log(request)
+      const duration = parseInt(request.duration)
+      chrome.storage.local.set({ duration }, () => {
+        timeoutDuration = duration
+      })
+      chrome.storage.local.get(['openTabs'], (result) => {
+        const { openTabs } = result
+        chrome.windows.getCurrent({ populate: true }, (window) => {
+          window.tabs.forEach(tab => {
+            clearTimeoutForTab(tab.id)
+            if (!tab.active && !openTabs.includes(tab.id)) {
+              setTimeoutForTab(tab.id, duration)
+            }
+          })
+        })
+      })
+    }
+    sendResponse('RECEIVED')
+  });
+
+function checkboxClick(cb) {
+  new Promise(res => {
+    chrome.storage.local.get(['openTabs', 'currentTab', 'duration'], ({ openTabs, currentTab, duration }) => {
+      if (openTabs.includes(currentTab)) {
+        openTabs = openTabs.filter(tab => {
+          tab === currentTab
+        })
+        setTimeoutForTab(currentTab, duration)
+      }
+      else {
+        openTabs.push(currentTab)
+        clearTimeoutForTab(currentTab)
+      }
+      res(openTabs)
+    })
+  }).then(openTabs => {
+    chrome.storage.local.set({ openTabs }, () => {
+      console.log('setOpenTabs', openTabs)
+    })
+  })
+
+}
+
+function setTimeoutForTab(tab, timeoutDuration) {
+  timeouts[tab] = setTimeout(() => {
+    chrome.tabs.remove(tab)
+  }, timeoutDuration)
+}
+
+function clearTimeoutForTab(tab) {
+  clearTimeout(timeouts[tab])
+  delete timeouts[tab]
+}
--- popup.js
+++ popup.js
@@ -1,1 +1,52 @@
-chrome.tabs.getCurrent(tab => console.log(tab))
+$(document).ready(() => {
+    $('select').formSelect();
+    const durationSelect = $('#duration')
+    chrome.storage.local.get(['duration'], (result) => {
+        $('option:selected').removeAttr('selected')
+        $('#duration option[value="' + result.duration + '"]').prop('selected', true)
+    })
+
+    const keepGroupOpenInput = $('#keepGroupOpen')
+    deriveInputState(keepGroupOpenInput)
+
+    const keepOpenInput = $('#keepOpen')
+    deriveInputState(keepOpenInput)
+
+    keepOpenInput.click(() => {
+        chrome.runtime.sendMessage({
+            keepOpen: 'CLICK'
+        }, (response) => {
+            console.log(response)
+        })
+    })
+
+    durationSelect.change(() => {
+        chrome.runtime.sendMessage({
+            duration: $("select#duration option:selected").val()
+        }, (response) => {
+            console.log(response)
+        })
+    })
+
+
+})
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        console.log(request, sender, sendResponse)
+    }
+)
+
+function deriveInputState(node) {
+    const id = node.attr('id')
+    console.log(id)
+    if (id === 'keepOpen') {
+        chrome.storage.local.get(['openTabs', 'currentTab'], (response) => {
+            node.prop('checked', response.openTabs.includes(response.currentTab))
+        })
+    }
+    if (id === 'keepGroupOpen') {
+        console.log(node.id)
+    }
+
+}
